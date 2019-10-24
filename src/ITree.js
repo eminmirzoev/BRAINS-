5c52adbd9eea25ec34b4afb2b6d7784d181e6a5d
elmardeveducation <elmarh@deveducation.com>
2019-10-24 12:11:27 +0400

    Interface ITree has been created

---
+++ src/ITree.js
@@ -0,0 +1,16 @@
+function ITree() {};
+
+ITree.prototype.init = function(array) { console.log('ITree.init'); }
+ITree.prototype.clear = function() { console.log('ITree.clear'); }
+ITree.prototype.getSize() = function(array) { console.log('ITree.getSize'); }
+ITree.prototype.toArray = function() { console.log('ITree.toArray'); }
+ITree.prototype.toString = function() { console.log('ITree.toString'); }
+ITree.prototype.add = function(value) { console.log('ITree.add'); }
+ITree.prototype.delete = function(value) { console.log('ITree.delete'); }
+ITree.prototype.getWidth = function() { console.log('ITree.getWidth'); }
+ITree.prototype.getHeight = function() { console.log('ITree.getHeight'); }
+ITree.prototype.getNodes = function() { console.log('ITree.getNodes'); }
+ITree.prototype.getLeaves = function() { console.log('ITree.getLeaves'); }
+ITree.prototype.reverse = function() { console.log('ITree.reverse'); }
+
+module.exports = ITree;
